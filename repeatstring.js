8ec3d3c55cf91b8e2ca4d7ebff5be5e12c367dab
Cassandra King <puddincat@gmail.com>
2018-05-02 20:47:21 -0400

    repeat and recursion

--- repeatstring.js
+++ repeatstring.js
@@ -6,16 +6,27 @@
 //   }
 // }
 
+// function repeatStringNumTimes(string,number){
+//   let finalString = ""
+//   if (number < 0) {
+//     return ""
+//   } else {
+//     for( let i=0; i<number;i++){
+//       finalString+=string
+//     }
+//   }
+//   return finalString
+// }
+
+
 function repeatStringNumTimes(string,number){
-  let finalString = ""
-  if (number < 0) {
-    return ""
-  } else {
-    for( let i=0; i<number;i++){
-      finalString+=string
-    }
-  }
-  return finalString
+if (number < 0 ) {
+  return ""
+} else if (number ===1) {
+  return string
+} else {
+  return string += repeatStringNumTimes(string, number -1)
+}
 }
 
 console.log(repeatStringNumTimes("hello", 3))
